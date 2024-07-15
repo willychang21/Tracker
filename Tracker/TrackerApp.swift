d02d2c6eb3f0b3100b6d7f0da16aa9f76b9b92a2
willychang21 <willychang17@gmail.com>
2024-07-14 20:49:38 -0500

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by 張竣傑 on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
